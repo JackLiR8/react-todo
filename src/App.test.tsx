a54b3264ceccdee542926d82c1de039442949355
JackLiR8 <jiakeli1024@gmail.com>
2023-03-02 17:27:31 +0800

    feat: day 1
    add submit button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders submit button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const buttonElement = screen.getByRole('button');
+  expect(buttonElement).toBeInTheDocument();
+  expect(buttonElement.innerHTML).toBe('Submit')
 });
